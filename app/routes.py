38fcc889e7ae013bf7e3b39d1c560127a24d46e1
jhonny rojas <jhonnyrojasflo@gmail.com>
2025-05-21 22:03:28 -0400

    terminado

--- app/routes.py
+++ app/routes.py
@@ -108,19 +108,171 @@ def logs():
         logs = []
     return render_template("logs.html", logs=logs, tipo_seleccionado=tipo)
 
+
+@bp.route('/reportes_apache')
+def Reportes_apache():    
+    return render_template('reportes_apache.html')
+@bp.route('/reportes_ftp')
+def Reportes_ftp():    
+    return render_template('reportes_ftp.html')
+
+
+@bp.route('/api/reportes_ftp')
+def ftp_data():
+    cur = app.mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+    # 1. Acciones por tipo
+    cur.execute("""
+        SELECT tipo_accion, COUNT(*) AS cantidad
+        FROM ftp_logs
+        GROUP BY tipo_accion
+        ORDER BY cantidad DESC
+    """)
+    tipos_accion = cur.fetchall()
+
+    # 2. Top 10 usuarios
+    cur.execute("""
+        SELECT usuario, COUNT(*) AS acciones
+        FROM ftp_logs
+        GROUP BY usuario
+        ORDER BY acciones DESC
+        LIMIT 10
+    """)
+    top_usuarios = cur.fetchall()
+
+    cur.close()
+
+    return jsonify({
+        "tipos_accion": tipos_accion,
+        "top_usuarios": top_usuarios
+    })
+    
+
+@bp.route('/api/datos_access')
+def datos_access():
+    cur = app.mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    # 1. Obtener códigos de estado
+    cur.execute("""
+        SELECT codigo_estado, COUNT(*) AS cantidad
+        FROM apache_access_logs
+        GROUP BY codigo_estado
+        ORDER BY cantidad DESC;
+    """)
+    codigos_estado = cur.fetchall()
+
+    # 2. Obtener métodos HTTP
+    cur.execute("""
+        SELECT metodo, COUNT(*) AS cantidad
+        FROM apache_access_logs
+        GROUP BY metodo
+        ORDER BY cantidad DESC;
+    """)
+    metodos_http = cur.fetchall()
+
+    # 3. Obtener top IPs
+    cur.execute("""
+        SELECT ip, COUNT(*) AS requests
+        FROM apache_access_logs
+        GROUP BY ip
+        HAVING requests > 10
+        ORDER BY requests DESC
+        LIMIT 10;
+    """)
+    top_ips = cur.fetchall()
+
+    # 4. Obtener recursos más solicitados
+    cur.execute("""
+        SELECT recurso AS path, COUNT(*) AS count
+        FROM apache_access_logs
+        GROUP BY recurso
+        ORDER BY count DESC
+        LIMIT 10;
+    """)
+    top_paths = cur.fetchall()
+
+    # Cerrar cursor
+    cur.close()
+
+    return jsonify({
+        "codigos_estado": codigos_estado,
+        "metodos_http": metodos_http,
+        "top_ips": top_ips,
+        "top_paths": top_paths
+    })
+    
+
+@bp.route('/api/buscador', methods=['POST'])
+def buscador():
+    datos = request.get_json()
+    tipo = datos.get('select_categoria')
+    termino_buscado = datos.get('text_buscador').strip()
+    print(datos)
+    cur = app.mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    if tipo == 'apache_access'and termino_buscado:
+            query = """
+                SELECT * FROM apache_access_logs
+                WHERE 
+                    ip LIKE %s OR
+                    usuario LIKE %s OR
+                    metodo LIKE %s OR
+                    recurso LIKE %s OR
+                    protocolo LIKE %s OR
+                    user_agent LIKE %s OR
+                    codigo_estado LIKE %s OR
+                    tamano_respuesta LIKE %s 
+
+            """
+            like_term = f"%{termino_buscado}%"
+            cur.execute(query, (like_term, like_term, like_term, like_term, like_term, like_term,like_term, like_term))
+            logs = cur.fetchall()
+            cur.close()
+    elif tipo == 'apache_error' and termino_buscado :
+            query = """
+                SELECT * FROM apache_error_logs
+                WHERE 
+                    nivel LIKE %s OR
+                    modulo LIKE %s OR
+                    pid_id LIKE %s OR
+                    codigo_mensaje LIKE %s OR
+                    mensaje LIKE %s
+
+            """
+            like_term = f"%{termino_buscado}%"
+            cur.execute(query, (like_term, like_term, like_term, like_term, like_term))
+            logs = cur.fetchall()
+            cur.close()
+    elif tipo == 'ftp' and termino_buscado:
+            query = """
+                SELECT * FROM ftp_logs
+                WHERE 
+                    usuario LIKE %s OR
+                    pid_id LIKE %s OR
+                    ip LIKE %s OR
+                    tipo_accion LIKE %s OR
+                    descripcion LIKE %s
+            """
+            like_term = f"%{termino_buscado}%"
+            cur.execute(query, (like_term, like_term, like_term, like_term, like_term))
+            logs = cur.fetchall()
+            cur.close()
+            
+    else:
+             logs = datos
+    resultados = {
+        "mensaje": "Filtros recibidos correctamente",
+        "datos_filtrados": logs,
+    }
+    return jsonify(resultados)
+
+
 @bp.route('/api/filtros', methods=['POST'])
 def filtrar():
     datos = request.get_json()
     cur = app.mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    # Extraer los filtros enviados
-    fecha = datos.get('fecha')
-    fecha_inicio = datos.get('fechaInicio')
-    fecha_fin = datos.get('fechaFin')
-    hora = datos.get('hora')
+
     tipo = datos.get('select_categoria')
     print(tipo)
-    categorias_seleccionadas = datos.get('categorias_selecionadas')
-    print(datos)
+
     if tipo == 'apache_access':
        sql = construir_sql_access(datos)
        cur.execute(sql)
@@ -234,7 +386,7 @@ def construir_sql_access(filtros):
         codigos_str = "', '".join(codigos)
         condiciones.append(f"codigo_estado IN ('{codigos_str}')")
 
-    navegadores = categorias.get('Codigo Estado', [])
+    navegadores = categorias.get('Navegador', [])
     if navegadores:
         navegadores_ua = [f"user_agent LIKE '%{ua}%'" for ua in navegadores]
         condiciones.append("(" + " OR ".join(navegadores_ua) + ")")
